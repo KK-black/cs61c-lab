d68ffe2022032989e6f91f6b7d0e20de6c309470
kily <1770693880@qq.com>
2023-04-09 11:27:43 +0800

    lab3

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,15 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *fast = head;
+    node *slow = head;
+    
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            return 1;
+        }
+    }
     return 0;
 }
---
+++ lab01/ptr.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+
+    int *ptr;
+    *ptr = 5;
+
+    printf("%d" , *ptr);
+    return 0;
+}
